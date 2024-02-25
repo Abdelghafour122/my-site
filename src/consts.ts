b13cca0ebd67b01017bbc21d9e0da6ed27d651f6
Abdelghafour <ghafourragg@gmail.com>
2024-02-25 19:37:50 +0100

    added an Icon enum

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,21 @@
 
 export const SITE_TITLE = "Abdelghafour Echikr | Web Developer";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export enum IconEnum {
+  linkedIn = "linkedin",
+  github = "github",
+  css = "css",
+  javascript = "javascript",
+  dz = "dzflag",
+  hashnode = "hashnode",
+  logo = "logo",
+  mail = "mail",
+  nextjs = "next-js",
+  node = "nodejs",
+  react = "react",
+  sass = "sass",
+  tailwind = "tailwindcss",
+  twitter = "twitter",
+  typescript = "typescript",
+}
