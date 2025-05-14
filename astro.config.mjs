c40c4d125c67ed55007cf6d38401b3be9416a69d
Abdelghafour122 <ghafourragg@gmail.com>
2025-05-14 17:11:20 +0100

    updated tailwind to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind(), icon()]
+  integrations: [mdx(), sitemap(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
